6bef3077933699236598659b92574dd3d33ce155
Ernesto <ernestrc@localhost.localdomain>
2017-02-06 23:42:40 -0600

    WIP

--- Sistemas Embebidos/01_scheduler_cooperative/src/ECU Abstraction/LIN/lin_types.h
+++ Sistemas Embebidos/01_scheduler_cooperative/src/ECU Abstraction/LIN/lin_types.h
@@ -5,12 +5,12 @@
 
 typedef uint8_t LinFramedPidType;  	/*  LIN frame identifier along with its two parity bits. Range: 0xFE */
 typedef enum _LinFrameCsModelType_t_{
-	LIN_ENHANCED_CS;				/*   Enhance frame Checksum, add PID to the equation   */
+	LIN_ENHANCED_CS,				/*   Enhance frame Checksum, add PID to the equation   */
 	LIN_CLASSIC_CS					/*   Classic frame Checksum   */
 }LinFrameCsModelType_t;
 typedef enum _LinFrameResponseType_t_{
-	LIN_MASTER_RESPONSE;			/*   Response is generated from this node (Master)   */
-	LIN_SLAVE_RESPONSE;				/*   Response is generated from a remote slave mode   */
+	LIN_MASTER_RESPONSE,			/*   Response is generated from this node (Master)   */
+	LIN_SLAVE_RESPONSE				/*   Response is generated from a remote slave mode   */
 }LinFrameResponseType_t;			
 typedef uint8_t LinFrameDlType;		/*   Number of SDU of data bytes to copy   */
 typedef struct _LinPduType_t_{
